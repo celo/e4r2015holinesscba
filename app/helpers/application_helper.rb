06303b4b8efe3a84874b270bf5ce1ce52c2b2b45
Marcelo Mogami <marcelomogami@gmail.com>
2015-02-03 12:44:46 +0000

    Improved static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Encontro das Igrejas Holiness da 4&ordf; Regi&atilde;o"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
