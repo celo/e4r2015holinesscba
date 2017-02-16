9f1f634be0ea40ecf2e2bfe12e5a03760a1c709c
Marcelo Mogami <marcelomogami@gmail.com>
2017-02-16 15:46:07 +0000

    Changed Event tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   it 'has a valid factory'
-  it 'is invalid without name'
-  it 'is invalid withuot start_date'
-  it 'is invalid without end_date'
-  it 'is invalid if end_date is earlier than start_date'
+  it 'has a valid name'
+  it 'has a valid start_date'
+  it 'has a valid end_date'
+  it 'has a start_date ealier than end_date'
 end
