d0e6763a47eb2f14d1700b8beb88237a401f0daa
Marcelo Mogami <marcelomogami@gmail.com>
2017-02-18 01:20:57 +0000

    States and Cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216034921) do
+ActiveRecord::Schema.define(version: 20170218011204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.integer  "state_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id", using: :btree
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -23,4 +31,12 @@ ActiveRecord::Schema.define(version: 20170216034921) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "states", force: :cascade do |t|
+    t.string   "name"
+    t.string   "abbr"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cities", "states"
 end
