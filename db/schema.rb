6eb02cc6aebdd228945c3c157889b2f43521d61b
Marcelo Mogami <marcelomogami@gmail.com>
2017-03-09 03:32:09 +0000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218011204) do
+ActiveRecord::Schema.define(version: 20170218014256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "churches", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "abbr"
+    t.index ["city_id"], name: "index_churches_on_city_id", using: :btree
+  end
 
   create_table "cities", force: :cascade do |t|
     t.integer  "state_id"
@@ -38,5 +47,6 @@ ActiveRecord::Schema.define(version: 20170218011204) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "churches", "cities"
   add_foreign_key "cities", "states"
 end
